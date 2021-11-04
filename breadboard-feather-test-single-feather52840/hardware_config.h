69decec1fd3dcb4cf7cca2586ae75a82c2d569f3
Evelyn Dooley <31457788+evelyndooley@users.noreply.github.com>
2021-11-04 15:53:15 -0400

    Update hardware_config.h

--- breadboard-feather-test-single-feather52840/hardware_config.h
+++ breadboard-feather-test-single-feather52840/hardware_config.h
@@ -23,16 +23,28 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 
 /* HARDWARE DEFINITION*/
 /* key matrix size */
-#define MATRIX_ROWS 1
-#define MATRIX_COLS 21
+/* HARDWARE DEFINITION*/
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 4
 
-  #define MATRIX_ROW_PINS { 33 } // 33 is a not connected pin
-  #define MATRIX_COL_PINS { 14, 15, 16, 17, 18, 19, 26, 25, 24, 1, 0, 2, 22, 23, 5, 6, 9, 10, 11, 12, 13 }
-
+#define MATRIX_ROW_PINS {7, 26, 27, 6, 8 }
+#define MATRIX_COL_PINS {4, 5, 30, 28 }
 #define UNUSED_PINS {}
-
+// #define BACKLIGHT_LED_PIN 38 
+// #define DEFAULT_PWM_VALUE 10000            // PWM intensity
+// #define BACKLIGHT_PWM_ON 1
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
+#define BATTERY_TYPE BATT_LIPO
+#define VBAT_PIN  29
+
+#define  STATUS_BLE_LED_PIN  10  //blue = 0.19
+#define  STATUS_KB_LED_PIN 15  //red = 0.17
+
+#define I2C_SDA_PIN 12
+#define I2C_SCK_PIN 11
+#define DISPLAY_U8G2_CONSTRUCTOR U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C // see https://github.com/olikraus/u8g2/wiki/u8g2setupcpp for reference
 
     
 #endif /* HARDWARE_CONFIG_H */
